1e554108436264cbec4a5c4d95add76081f2b593
danielsGrietins <daniels.grietins@gmail.com>
2017-11-24 10:58:48 +0200

    Route fix

--- Http/routes.php
+++ Http/routes.php
@@ -8,4 +8,7 @@ Route::group([
     Route::resource('api/crip-files', '\Crip\Filesys\App\Controllers\FileController');
     Route::get('api/crip-tree', '\Crip\Filesys\App\Controllers\TreeController');
     Route::get('/', '\Crip\Filesys\App\Controllers\ManagerController');
+
+    Route::get('api/crip-folders/{location?}', '\Crip\Filesys\App\Controllers\FolderController@show')->where('location', '(.*)');
+    Route::get('api/crip-files/{location?}', '\Crip\Filesys\App\Controllers\FileController@show')->where('location', '(.*)');
 });
