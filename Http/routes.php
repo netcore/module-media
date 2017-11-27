2e5bb92eb1ff1a2b669030fad55e5a2e91cc7902
danielsGrietins <daniels.grietins@gmail.com>
2017-11-27 12:05:06 +0200

    Updated routes 2

--- Http/routes.php
+++ Http/routes.php
@@ -4,6 +4,11 @@ Route::group([
     'prefix'     => 'admin/media/packages/filemanager',
     'as'         => 'admin::media.',
 ], function () {
+    Route::resource('api/crip-folders', '\Crip\Filesys\App\Controllers\FolderController');
+    Route::resource('api/crip-files', '\Crip\Filesys\App\Controllers\FileController');
+    Route::get('api/crip-tree', '\Crip\Filesys\App\Controllers\TreeController');
+    Route::get('/', '\Crip\Filesys\App\Controllers\ManagerController');
+
     Route::get('api/crip-folders/{location?}', '\Crip\Filesys\App\Controllers\FolderController@show')->where('location', '(.*)');
     Route::patch('api/crip-folders/{location?}', '\Crip\Filesys\App\Controllers\FolderController@update')->where('location', '(.*)');
     Route::delete('api/crip-folders/{location?}', '\Crip\Filesys\App\Controllers\FolderController@destroy')->where('location', '(.*)');
